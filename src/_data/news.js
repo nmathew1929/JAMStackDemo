cc85127b4f9f30158a3a31c909dcb25f28e87442
navin <navinmathew3@gmail.com>
2021-02-21 19:44:47 -0600

    added redirects and localized pages for countries

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,29 @@
 const { default: axios } = require("axios")
+const countries = require("./countries.json")
+
 require('dotenv').config();
 
-module.exports = async function() {
-    url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+
+async function getNews(country) {
     try {
-        const response = await axios.get(url)
-        return response.data;
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
+        return {
+           "country": country,
+            "articles": response.data.articles
+        }
     } catch (error) {
         console.error(error);
     }
+}
+
+
+
+
+module.exports = async function() {
+    var newPromises = countries.map(getNews);
+    return Promise.all(newPromises).then(newsObject => {
+        console.log('newsObjects: ', newsObject);
+        return[].concat.apply([], newsObject);
+    })
 }
 
